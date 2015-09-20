16b49d069dd6fe461963ffab8bef68eacb2c96ad
Brandon Rodriguez <brodriguez8774@mail.kvcc.edu>
2015-09-19 23:12:54 -0400

    Creates basic interface.

--- app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
@@ -4,13 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CounterActivity extends AppCompatActivity {
+
+    private Button mCounterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+
+        mCounterButton = (Button) findViewById(R.id.counter_button);
+        mCounterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CounterActivity.this, R.string.gather_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
